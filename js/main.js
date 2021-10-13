bbc2f1b1774ef11bb57465011a659bc17e4e2891
Kim Jung Hak <lovebox0310@gmail.com>
2021-10-13 22:18:28 +0900

    게임종료: 게임종료시 모달팝업이 활성화되고 다시시작 버튼을 추가하여 새로시작 할수 있도록 소스 추가함.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ import BLOCKS from './blocks.js'
 
 // DOM
 const playground = document.querySelector('.playground > ul')
+const gametext = document.querySelector('.game-text')
+const gamestoptext = document.querySelector('.game-stop-text')
+const restartbutton = document.querySelector('.game-text > button')
+const stopRestartButton = document.querySelector('.game-stop-text > button')
+const scoreText = document.querySelector('.score')
 
 // Setting
 const GAME_ROWS = 20
@@ -14,7 +19,7 @@ let downInterval
 let tempMovinngItem
 
 const movingItem = {
-  type: 'elRight',
+  type: '',
   direction: 0,
   top: 0,
   left: 0,
@@ -27,7 +32,7 @@ const init = () => {
   for (let i = 0; i < GAME_ROWS; i++) {
     prependNewLine()
   }
-  renderBlocks()
+  generateNewBlock()
 }
 
 const prependNewLine = () => {
@@ -57,9 +62,13 @@ const renderBlocks = (moveType = '') => {
     if (isAvailable) {
       target.classList.add(type, 'moving')
     } else {
+      if (moveType === 'retry') {
+        clearInterval(downInterval)
+        showGameOverText()
+      }
       tempMovinngItem = { ...movingItem }
       setTimeout(() => {
-        renderBlocks()
+        renderBlocks('retry')
         if (moveType === 'top') {
           seizeBlock()
         }
@@ -72,16 +81,46 @@ const renderBlocks = (moveType = '') => {
   movingItem.direction = direction
 }
 
+const showGameOverText = () => {
+  gametext.style.display = 'flex'
+}
+
 const seizeBlock = () => {
   const movingBlocks = document.querySelectorAll('.moving')
   movingBlocks.forEach((moving) => {
     moving.classList.remove('moving')
     moving.classList.add('seized')
   })
+  checkMatch()
+}
+
+const checkMatch = () => {
+  const childNodes = playground.childNodes
+  childNodes.forEach((child) => {
+    let matched = true
+    child.childNodes[0].childNodes.forEach((li) => {
+      if (!li.classList.contains('seized')) {
+        matched = false
+      }
+    })
+    if (matched) {
+      child.remove()
+      prependNewLine()
+      chagneScore()
+    }
+  })
   generateNewBlock()
 }
 
+const chagneScore = () => {
+  scoreText.innerHTML = score += 10
+}
+
 const generateNewBlock = () => {
+  clearInterval(downInterval)
+  downInterval = setInterval(() => {
+    moveBlock('top', 1)
+  }, duration)
   const blockArray = Object.entries(BLOCKS)
   const randomIndex = Math.floor(Math.random() * blockArray.length)
   movingItem.type = blockArray[randomIndex][0]
@@ -112,6 +151,18 @@ const chageDirection = () => {
   renderBlocks()
 }
 
+const droplook = () => {
+  clearInterval(downInterval)
+  downInterval = setInterval(() => {
+    moveBlock('top', 1)
+  }, 10)
+}
+
+const gameStop = () => {
+  clearInterval(downInterval)
+  gamestoptext.style.display = 'flex'
+}
+
 // event handling
 document.addEventListener('keydown', (e) => {
   switch (e.keyCode) {
@@ -127,6 +178,9 @@ document.addEventListener('keydown', (e) => {
     case 38:
       chageDirection()
       break
+    case 32:
+      droplook()
+      break
     default:
       break
   }
@@ -134,3 +188,10 @@ document.addEventListener('keydown', (e) => {
 })
 
 init()
+
+// event
+restartbutton.addEventListener('click', () => {
+  playground.innerHTML = ''
+  gametext.style.display = 'none'
+  init()
+})
